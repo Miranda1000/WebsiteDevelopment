7c1c9ca142a86d0ec990f37b8737fab1eb1bb180
Miranda1000 <miranda-a-100@hotmail.com>
2022-06-02 21:09:52 +0100

    Basic structure of website with topnav and icons

--- MainPageJavascript.js
+++ MainPageJavascript.js
@@ -1,3 +1,4 @@
+/*Responsive topnav */
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
